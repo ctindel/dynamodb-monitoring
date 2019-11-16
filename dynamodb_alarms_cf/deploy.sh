13dc7618193f0897feecaf9ed3612c997c3e3583
Chad Tindel <chad@tindel.net>
2019-11-16 05:49:15 +0000

    added a comment

--- dynamodb_alarms_cf/deploy.sh
+++ dynamodb_alarms_cf/deploy.sh
@@ -6,14 +6,15 @@
 : "${DYNAMODB_SNS_EMAIL:?Need to set DYNAMODB_SNS_EMAIL to valid email address}"
 
 EMAIL_REGEX="^[a-z0-9!#\$%&'*+/=?^_\`{|}~-]+(\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@([a-z0-9]([a-z0-9-]*[a-z0-9])?\.)+[a-z0-9]([a-z0-9-]*[a-z0-9])?\$"
+STACK_NAME=dynamodb-monitoring
 
 if [[ ! $DYNAMODB_SNS_EMAIL =~ $EMAIL_REGEX ]] ; then
     echo "Need to set DYNAMODB_SNS_EMAIL to valid email address"
     exit 1
 fi
 
-STACK_NAME=dynamodb-monitoring
-
+# I coudln't figure out a way to put most of the variables in a params file and still pass in the email parameter from the environment
+#  If anybody knows a way, it would be nice to know.
 if ! aws cloudformation describe-stacks --stack-name $STACK_NAME > /dev/null 2>&1; then
     aws cloudformation create-stack --stack-name $STACK_NAME --template-body file:///tmp/src/dynamodb_alarms_cf/dynamodb_alarms_cf.yaml --parameters \
         ParameterKey=DynamoDBProvisionedTableName,ParameterValue=dynamodb-provisioned-monitoring \
